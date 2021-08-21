3ed11bdb696156161a642ffef8c685e6b67aa77a
cmmerritt <christiane.merritt@gmail.com>
2021-08-20 23:05:14 -0700

    hard code some goats

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,14 +1,43 @@
 import * as React from "react"
 import '../styles/index-styles.sass';
-
+import blackphillip from '../images/black-phillip.jpeg'
+import jurassicgoat from '../images/jurassicgoat.jpeg'
 
 const IndexPage = () => {
   return (
-    <main>
-      <title>Goats on Film</title>
-      <h1>Goats on Film</h1>
-      <p>Some notable goats</p>
-    </main>
+    <body>
+      <header>Goats on Film</header>
+      <main>
+        <title>Goats on Film</title>
+        <p>Some notable goats</p>
+        <ul>
+          <li>
+            <h4>Black Phillip</h4>
+            <span>The Witch</span>
+            <p>dir. Robert Eggers, 2015</p>
+            <img src={blackphillip} alt="Black Phillip"></img>
+            <p>Wouldst thou like to live deliciously?</p>
+            <p>Rating:
+              <span>
+                🐐🐐🐐🐐🐐
+              </span>
+            </p>
+          </li>
+          <li>
+            <h4>Anonymous Goat</h4>
+            <span>Jurassic Park</span>
+            <p>dir. Steven Spielberg, 1994</p>
+            <img src={jurassicgoat} alt="Jurassic Park Goat"></img>
+            <p>This goat has nothing to say.</p>
+            <p>Rating:
+              <span>
+                🐐🐐🐐
+              </span>
+            </p>
+          </li>
+        </ul>
+      </main>
+    </body>
   )
 }
 
